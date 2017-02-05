b806d0c5d3ee70a4ed76a8527b28b945cffeca03
zafarluni <zafarluni@yahoo.com>
2017-02-05 21:30:33 +0300

    made changes for heroku

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+    if(req.headers['x-forwarded-proto'] == 'http'){
+        next();
+    }else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-    console.log("Application runnig");
+app.listen(PORT, () => {
+    console.log("Application runnig...");
 });
